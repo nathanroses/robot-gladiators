05b02c2398bdb8d590052281ae3ce3ea16e9aa59
nathanroses <87315662+nathanroses@users.noreply.github.com>
2021-08-27 23:28:52 -0500

    updated section 1 game

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,16 +13,30 @@ var enemyHealth = "50";
 var enemyAttack =  "12";
 
 var fight = function() {
-  //Alert player that they are starting the round
+  
+    //Alert player that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
-  //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and us that result to update the value in the 'enemyHealth' variable
+  
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and us that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
 
-  //Log a resulting message to the console so we know that it worked
+  
+    //Log a resulting message to the console so we know that it worked
     console.log(
         playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
     );
+  
+    // check enemy's health
+     if (enemyHealth <= 0) {
+         window.alert(enemyName + " has DIED! " );
+     }   
+     
+     else {
+         window.alert(enemyName + " stil has " +  enemyHealth  + "  health left. ");
+     }
+
+
   //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
     playerHealth = playerHealth - enemyHealth;
 
@@ -30,7 +44,22 @@ var fight = function() {
      console.log(
          enemyName + " attacked " +  playerName  + " . " + playerName + " now has " +  playerHealth + " health remaining. "
    );
+  
+  //Check Players Health
+    
+   if (playerHealth <= 0) {
+         window.alert(playerName + " has DIED! ");
+     }
+  
+    else {
+         window.alert(playerName + " still has " + playerHealth + " health left ");
+  }
+
+
+
+
 };
+  
 
 
 
