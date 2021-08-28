1359b9d4c61d125d25a0807f4a3483dbc7e19b72
nathanroses <87315662+nathanroses@users.noreply.github.com>
2021-08-27 19:11:18 -0500

    building game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,38 @@
-window.alert("This is an alert! JavaScript is a Cript Member")
+// this creates a function alet named "fight"
+
+function fight() {
+    window.alert ("The fight has begun!");
+}
+                                              
+//this creates a prompt asking for "robots name"
+
+ var playerName = window.prompt("What is your robot's name?")
+
+//this displays inputed "player name"
+
+ window.alert(playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Function Call
+
+// fight();
